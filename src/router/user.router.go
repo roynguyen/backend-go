1750083fe492cc4e7bb0ba9e265a0df4c3b533e4
dangquan.nguyen <dangquan.nguyen@vexere.com>
2018-12-19 17:28:53 +0700

    code

--- src/router/user.router.go
+++ src/router/user.router.go
@@ -6,13 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func InitRouter() { //*mux.Router {
-	/*router := mux.NewRouter()
-	router.HandleFunc("/auth", authentication.CreateTokenEndpoint).Methods("POST")
-	router.HandleFunc("/eth/{address}/balance", authentication.ValidateMiddleware(controller.GetBalance)).Methods("GET")
-	router.HandleFunc("/eth/{address}/nonce", controller.GetNonce).Methods("GET")
-	router.HandleFunc("/eth/{address}", controller.GetAccount).Methods("GET")
-	return router*/
+func InitRouter() 
 	router := gin.Default()
 	v1 := router.Group("/eth")
 	{
